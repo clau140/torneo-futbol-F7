3d294a823d19a9b3ae2ba020f01bdfda914263e7
clau140 <clau.alberdi89@gmail.com>
2025-07-01 21:41:40 -0300

    Agrego video de fondo en home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,25 @@
 const Home = () => {
   return (
     <div className="relative h-screen overflow-hidden">
-     {/*
-     <video
+      <video
         className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
         autoPlay
         loop
         muted
         playsInline
       >
-        <source src="/fondoHome.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dt0evvaa1/video/upload/v1751416736/fondoInicio_pf41yg.mp4"
+          type="video/mp4"
+        />
         Tu navegador no soporta videos en HTML5.
-      </video> 
-      */}
-
+      </video>
 
       <div className="flex flex-col items-center justify-center h-full text-white bg-black/60">
         <h1 className="text-5xl font-bold mb-4">UEFA Champions FRIG</h1>
-       
       </div>
     </div>
   );
 };
 
 export default Home;
-
